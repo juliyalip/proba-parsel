854ef079ae4ca99edae250951936e17372a2cbd7
Juliya Lipatova <575@ukr.net>
2020-11-01 10:31:58 +0200

    Добавляет из библиотеки throttle

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
-console.log('Привет, Мир!');
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-msg';
 
+const refs = {
+  form: document.querySelector('.js-feedback-form'),// dostychalis ko vsey forme
+  textarea: document.querySelector('.js-feedback-form  textarea'),// dostychalis k bolshomy polyu
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 5000));
+
+populateTextarea();
+
+/*
+ * - Останавливаем поведение по умолчанию
+ * - Убираем сообщение из хранилища
+ * - Очищаем форму
+ */
+function onFormSubmit(evt) {
+  
+  evt.preventDefault();
+
+  console.log('Отправляем форму');
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+/*
+ * - Получаем значение поля
+ * - Сохраняем его в хранилище
+ * - Можно добавить throttle
+ */
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+/*
+ * - Получаем значение из хранилища
+ * - Если там что-то было, обновляем DOM
+ */
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
+
+// Домой
+// сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
+
+// const formData = {};
+
+// refs.form.addEventListener('input', e => {
+//   // console.log(e.target.name);
+//   // console.log(e.target.value);
+
+//   formData[e.target.name] = e.target.value;
+
+//   console.log(formData);
+// });
